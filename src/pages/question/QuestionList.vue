1e76c16d3868bf4f9af250f212b202475c5fe6ca
tjdudtls312@gmail.com <=>
2024-01-12 18:11:09 +0900

    - 기본 question list

--- src/pages/question/QuestionList.vue
+++ src/pages/question/QuestionList.vue
@@ -1,19 +1,75 @@
 <script setup>
 import { computed, ref } from 'vue'
-import * as dayjs from 'dayjs'
 
-const count = ref(0)
-const inputValue = ref()
-const now = dayjs().format('YYYY.MM.DD HH:mm:ss')
+const columns = [
+      { name: 'questionSeq', label: '고유번호', field: 'questionSeq' },
+      { name: 'question', label: '질문', field: 'question' },
+      { name: 'difficulty', label: '난이도', field: 'difficulty' },
+      { name: 'type', label: '문제유형', field: 'type' },
+      { name: 'category', label: '카테고리명', field: 'category' },
+      { name: 'isShow', label: '사용 여부', field: 'isShow' }
+    ];
 
-const increment = () => {
-  count.value++
-}
+    const rows = [
+      {
+        questionSeq: 1,
+        question: '질문1',
+        difficulty: 1,
+        type: 'CHOICE',
+        category: 'Oracle',
+        isShow: true ? '사용' : '미사용'
+      },
+      {
+        questionSeq: 2,
+        question: '질문2',
+        difficulty: 2,
+        type: 'CHOICE',
+        category: '알고리즘',
+        isShow: true ? '사용' : '미사용'
+      },
+      {
+        questionSeq: 3,
+        question: '질문3',
+        difficulty: 3,
+        type: 'CHOICE',
+        category: '자바',
+        isShow: true ? '사용' : '미사용'
+      },
+      {
+        questionSeq: 4,
+        question: '질문4',
+        difficulty: 1,
+        type: 'CHOICE',
+        category: '자바스크립트',
+        isShow: true ? '사용' : '미사용'
+      },
+      {
+        questionSeq: 5,
+        question: '질문5',
+        difficulty: 1,
+        type: 'CHOICE',
+        category: '타입스크립트',
+        isShow: true ? '사용' : '미사용'
+      },
+      {
+        questionSeq: 6,
+        question: '질문6',
+        difficulty: 1,
+        type: 'CHOICE',
+        category: 'MySql',
+        isShow: true ? '사용' : '미사용'
+      }
+    ];
+
 </script>
 
 <template>
-  <button @click="increment">{{ count }}</button>
-  <input v-model="inputValue" />
-  value : {{ inputValue }} <br />
-  now : {{ now }} <br />
+  <div class="q-pa-md">
+    <q-table
+      title="문제"
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+    />
+  </div>
 </template>
